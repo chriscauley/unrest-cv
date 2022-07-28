4430728c054c7bdf6024b05dd046457ba52b6ea0
chriscauley <chris@lablackey.com>
2022-07-28 14:38:00 -0400

    get_exact_roi (prompts user with zoomed in corners)

--- urcv/input.py
+++ urcv/input.py
@@ -24,9 +24,40 @@ def wait_key(max_time=60000, default=None, delay = 500):
 
 
 def get_scaled_roi(image, scale):
-    w = image.shape[1] * scale
-    h = image.shape[0] * scale
+    w0 = image.shape[1] * scale
+    h0 = image.shape[0] * scale
     name = f'ROI_{random.random()}'
-    bounds = cv2.selectROI(name, cv2.resize(image, (w, h)))
+    x, y, w, h = cv2.selectROI(name, cv2.resize(image, (w0, h0), interpolation=cv2.INTER_NEAREST))
     cv2.destroyWindow(name)
-    return [int(i/scale) for i in bounds]
+
+    # get the rounded coords of the box
+    x2 = round((x + w) / scale)
+    y2 = round((y + h) / scale)
+    x1 = round(x / scale)
+    y1 = round(y / scale)
+
+    # return x, y, w, h based off scaled and rounded values
+    return [x1, y1, x2-x1, y2-y1]
+
+def get_exact_roi(image):
+    _image = image.copy()
+    x0, y0, w0, h0 = get_scaled_roi(image, 1)
+    _miny1 = max(y0-50, 0)
+    _minx1 = max(x0-50, 0)
+    topleft = _image[_miny1:y0+50,_minx1:x0+50]
+    x1, y1, _, _ = get_scaled_roi(topleft, 10)
+
+    x1 += _minx1
+    y1 += _miny1
+
+    x2 = x0 + w0
+    y2 = y0 + h0
+    _minx2 = x2-50
+    _miny2 = y2-50
+    bottomright = _image[_miny2:y2+50,_minx2:x2+50]
+    x2, y2, w2, h2 = get_scaled_roi(bottomright, 20)
+
+    x2 += _minx2 + w2
+    y2 += _miny2 + h2
+
+    return [x1, y1, x2 - x1, y2 - y1]
